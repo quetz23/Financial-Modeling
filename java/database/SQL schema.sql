c0325a14d4a46d7ef1371d663b06895c45b87363
Shomi <shomiedeki913@gmail.com>
2021-05-23 15:54:31 -0400

    Update SQL schema.sql

--- java/database/SQL schema.sql
+++ java/database/SQL schema.sql
@@ -2,27 +2,41 @@ BEGIN TRANSACTION;
 
 CREATE TABLE user_profile
 (
-profile_id INT GENERATED ALWAYS AS IDENTITY   PRIMARY KEY,
-user_id         int UNIQUE NOT NULL, 
+profile_id      int NOT NULL PRIMARY KEY,
+user_id         int UNIQUE NOT NULL,
 first_name      varchar(128)    NOT NULL,
 last_name       varchar(128)    NOT NULL,
-email           varchar(128)   NOT NULL
-
+email           varchar(128)   NOT NULL,
+recent_parameters varchar(900) PRIMARY KEY,
+watchlist_id           int NOT NULL PRIMARY KEY
 
 );
 
-CREATE TABLE stock_screener_parameters
+
+        
+
+CREATE TABLE recent_stock_screener_parameters
 (
+stock_screener_id    INT GENERATED ALWAYS AS IDENTITY,
+user_id                   int,
 market_cap_more_than      int,
 beta_more_than            int,    
 volume_more_than          int,
 sector          varchar(128),
-dividend_more_than        int
+dividend_more_than        int,
+CONSTRAINT fk_user_id FOREIGN KEY (user_id);
 );
 
 CREATE TABLE watchlist
 (
+watchlist_id int
 symbol varchar(10),
-company_name varchar(120)
+
 
 );
+
+COMMIT;
+
+ROLLBACK;
+
+
